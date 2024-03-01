a38aad49ea84f3a5e2695e1565d90433a9ce3e4d
gusgonnet <gusgonnet@gmail.com>
2024-02-29 21:22:24 -0500

    ionic: provide a credentials template file

---
+++ ionic/src/environments/credentialsTEMPLATE.ts
@@ -0,0 +1,10 @@
+// fill this file up with your web apps firebase configuration
+// then rename it to credentials.ts
+export const firebaseConfig = {
+    apiKey: "ksdhshlfd982349884792374189743",
+    authDomain: "your-proj-name.firebaseapp.com",
+    projectId: "your-proj-name",
+    storageBucket: "your-proj-name.appspot.com",
+    messagingSenderId: "029834092834029834",
+    appId: "092834ljkdfl0923423423jo234kl;24k3lkj234"
+};
