095e9db9ba9c70e5afbb965bf0a42404585a9ffc
gusgonnet <gusgonnet@gmail.com>
2024-06-21 08:45:26 -0400

    adding BLE buttons

--- particle/src/main.cpp
+++ particle/src/main.cpp
@@ -1,6 +1,7 @@
 #include "Particle.h"
 #include "edge.h"
 #include "monitor_edge_ioexpansion.h" // For general pin defines
+#include "BeaconScanner.h"
 
 SYSTEM_THREAD(ENABLED);
 SYSTEM_MODE(SEMI_AUTOMATIC);
@@ -9,6 +10,9 @@ SYSTEM_MODE(SEMI_AUTOMATIC);
 PRODUCT_ID(EDGE_PRODUCT_ID);
 #endif // EDGE_PRODUCT_NEEDED
 PRODUCT_VERSION(EDGE_PRODUCT_VERSION);
+
+#define BLACK_BUTTON "B4:35:22:28:AB:38"
+#define WHITE_BUTTON "5C:C7:C1:EE:37:D2"
 
 Ledger garageLedger;
 
@@ -101,5 +105,32 @@ void loop()
         digitalWrite(MONITOREDGE_IOEX_RELAY_OUT_PIN, HIGH);
         delay(1000);
         digitalWrite(MONITOREDGE_IOEX_RELAY_OUT_PIN, LOW);
+    }
+
+    static unsigned long scannedTime = 0;
+    if ((millis() - scannedTime) > 100)
+    {
+        Log.info("Scanning...");
+        scannedTime = millis();
+
+        Scanner.scan(1, SCAN_BTHOME);
+
+        Vector<BTHome> beacons = Scanner.getBTHome();
+        while (!beacons.isEmpty())
+        {
+            BTHome beacon = beacons.takeFirst();
+            Log.info("BTHome Address: %s, Battery: %d, Button: %d, Window: %d, Rotation: %d", beacon.getAddress().toString().c_str(), beacon.getBatteryLevel(), beacon.getButtonEvent(), beacon.getWindowState(), beacon.getRotation());
+
+            if (strcmp(beacon.getAddress().toString().c_str(), BLACK_BUTTON) == 0 || strcmp(beacon.getAddress().toString().c_str(), WHITE_BUTTON) == 0)
+            {
+                Log.info("beacon.getButtonEvent() = %d", beacon.getButtonEvent());
+
+                if (beacon.getButtonEvent() > 0)
+                {
+                    Log.info("Button pressed");
+                    toggleRelay = true;
+                }
+            }
+        }
     }
 }
